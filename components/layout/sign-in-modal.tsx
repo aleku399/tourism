67f80f362df44c72f4779e53662d3e85025cf9e9
alex <alex@alexs-MacBook-Pro.local>
2023-09-16 19:49:31 +0300

    Work on reviews

--- components/layout/sign-in-modal.tsx
+++ components/layout/sign-in-modal.tsx
@@ -8,7 +8,6 @@ import {
   useMemo,
 } from "react";
 import { LoadingDots, Google } from "@/components/shared/icons";
-import Image from "next/image";
 
 const SignInModal = ({
   showSignInModal,
@@ -23,20 +22,7 @@ const SignInModal = ({
     <Modal showModal={showSignInModal} setShowModal={setShowSignInModal}>
       <div className="w-full overflow-hidden shadow-xl md:max-w-md md:rounded-2xl md:border md:border-gray-200">
         <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center md:px-16">
-          <a href="https://precedent.dev">
-            <Image
-              src="/logo.png"
-              alt="Logo"
-              className="h-10 w-10 rounded-full"
-              width={20}
-              height={20}
-            />
-          </a>
           <h3 className="font-display text-2xl font-bold">Sign In</h3>
-          <p className="text-sm text-gray-500">
-            This is strictly for demo purposes - only your email and profile
-            picture will be stored.
-          </p>
         </div>
 
         <div className="flex flex-col space-y-4 bg-gray-50 px-4 py-8 md:px-16">
